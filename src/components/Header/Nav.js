6e8fc5f0e68a56fbb137a325662682ba0acc52b7
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-11 19:45:03 +0200

    Adding the Nav component

---
+++ src/components/Header/Nav.js
@@ -0,0 +1,76 @@
+import { useState, useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSearch,
+  faBars,
+  faToggleOn,
+  faToggleOff,
+} from '@fortawesome/free-solid-svg-icons';
+import Button from '../UI/Button/Button';
+import Search from './Search/Search';
+import Card from '../UI/Card/Card';
+import styles from './Nav.module.css';
+import ThemeContext from '../../store/themeContext';
+
+const Nav = ({ onThemeSelection }) => {
+  const [searchStatus, setsearchStatus] = useState(false);
+  const [asideStatus, setAsideStatus] = useState(false);
+  const [darkThemeEnabled, setDarkThemeEnabled] = useState(false);
+  const { darkTheme } = useContext(ThemeContext);
+
+  // TODO work on the aside (drawer)
+
+  const toggleSideHandler = () => {
+    setsearchStatus((prevState) => {
+      const asideToggle = prevState === false ? true : false;
+      setAsideStatus(asideToggle);
+    });
+  };
+
+  const toggleSearchHandler = (e) => {
+    e.preventDefault();
+    setsearchStatus((prevState) => {
+      const inputToggle = prevState === false ? true : false;
+      setsearchStatus(inputToggle);
+    });
+  };
+
+  const toggleThemeHandler = () => {
+    setDarkThemeEnabled((prevState) => {
+      const themeToggle = prevState === false ? true : false;
+      setDarkThemeEnabled(themeToggle);
+    });
+    onThemeSelection(darkThemeEnabled);
+  };
+
+  return (
+    <nav>
+      <Button
+        onClick={toggleSideHandler}
+        title="Toggle menu"
+        additionalClass={styles.cta}
+      >
+        <FontAwesomeIcon icon={faBars} />
+      </Button>
+      <Card className={styles.buttons}>
+        <form>
+          {searchStatus && <Search />}
+          <Button
+            onClick={toggleSearchHandler}
+            type="submit"
+            title="Search"
+            additionalClass={styles.cta}
+          >
+            <FontAwesomeIcon icon={faSearch} />
+          </Button>
+        </form>
+        <Button onClick={toggleThemeHandler} title="Toggle dark mode">
+          {darkTheme && <FontAwesomeIcon icon={faToggleOff} />}
+          {!darkTheme && <FontAwesomeIcon icon={faToggleOn} />}
+        </Button>
+      </Card>
+    </nav>
+  );
+};
+
+export default Nav;
