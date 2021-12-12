50cd4b6dd90ddd034edf0d403e19e15312c5df3f
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-12 21:47:28 +0200

    Adjusted the Header component to use context

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,25 @@
+import { useContext } from 'react';
+import ThemeContext from '../../store/themeContext';
 import styles from './Header.module.css';
 import Nav from './Nav';
 
-const Header = ({ onThemeSelection }) => {
-  const passTheme = (theme) => {
-    onThemeSelection(theme);
-  };
+const Header = () => {
+  const { darkTheme } = useContext(ThemeContext);
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${darkTheme && styles.dark}`}>
       <div className={styles.logoWrapper}>
         <a
           target="_blank"
           rel="noreferrer"
           href="https://www.github.com/developedbygeo"
         >
-          <h1>
+          <h1 className={darkTheme ? styles.darkText : undefined}>
             <span className={styles.logo1}>Task</span> Tracker
           </h1>
         </a>
       </div>
-      <Nav onThemeSelection={passTheme} />
+      <Nav />
     </header>
   );
 };
