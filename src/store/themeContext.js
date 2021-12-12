085af0e5a13e7794341c299fe3421cace9fcd8e2
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-12 21:46:14 +0200

    Refactored themeContext and added the themeContextProvider for modular state management

--- src/store/themeContext.js
+++ src/store/themeContext.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ThemeContext = React.createContext({ darkTheme: false });
+const ThemeContext = React.createContext({
+  darkTheme: false,
+  onThemeChange: () => {},
+});
+
+export const ThemeContextProvider = ({ children }) => {
+  const [darkThemeEnabled, setDarkThemeEnabled] = useState(false);
+
+  const themeChangeHandler = () => {
+    setDarkThemeEnabled((prevState) => {
+      const toggle = prevState === false ? true : false;
+      setDarkThemeEnabled(toggle);
+    });
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        darkTheme: darkThemeEnabled,
+        onThemeChange: themeChangeHandler,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 export default ThemeContext;
