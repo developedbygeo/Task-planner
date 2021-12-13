9d39ba96407f7dd9ec7908e8e689b67d96464bbd
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-13 21:15:48 +0200

    Code cleanup and now passing theme as props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,21 @@
-import { useContext } from 'react';
-import ThemeContext from '../../store/themeContext';
 import styles from './Header.module.css';
 import Nav from './Nav';
 
-const Header = () => {
-  const { darkTheme } = useContext(ThemeContext);
-
+const Header = ({ onAsideEnable, theme }) => {
   return (
-    <header className={`${styles.header} ${darkTheme && styles.dark}`}>
+    <header className={`${styles.header} ${theme && styles.dark}`}>
       <div className={styles.logoWrapper}>
         <a
           target="_blank"
           rel="noreferrer"
           href="https://www.github.com/developedbygeo"
         >
-          <h1 className={darkTheme ? styles.darkText : undefined}>
+          <h1 className={theme ? styles.darkText : undefined}>
             <span className={styles.logo1}>Task</span> Tracker
           </h1>
         </a>
       </div>
-      <Nav />
+      <Nav onAsideEnable={onAsideEnable} />
     </header>
   );
 };
