96c30cea5fcefcb22e5c1c22701a8302963eed6d
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-15 23:15:28 +0200

    Adding the updated App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import { useContext } from 'react';
-import Header from './components/Header/Header';
+import { useContext, useState } from 'react';
 import ThemeContext from './store/themeContext';
+import Header from './components/Header/Header';
+import Aside from './components/Aside/Aside';
+import Section from './components/Section/Section';
+import TaskList from './components/Tasks/TaskList';
 
 function App() {
   const ctx = useContext(ThemeContext);
+  const [asideStatus, setAsideStatus] = useState(false);
+
+  const asideHandler = () => {
+    setAsideStatus((prevState) => (prevState === false ? true : false));
+  };
 
   return (
     <>
-      <Header onThemeSelection={ctx.onThemeChange} />
-      <main className="App"></main>
+      <Header
+        onThemeSelection={ctx.onThemeChange}
+        onAsideEnable={asideHandler}
+      />
+      <main className="App">
+        {asideStatus && <Aside theme={ctx.darkTheme} />}
+        <Section>
+          <TaskList isAsideActive={asideStatus} />
+        </Section>
+      </main>
     </>
   );
 }
