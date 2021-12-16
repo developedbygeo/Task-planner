6d12a544336500f22b12c2f22711365a1683cb6d
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-16 17:58:55 +0200

    Added a task-adding button

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,13 +1,25 @@
 import { useContext } from 'react';
 import ThemeContext from '../../store/themeContext';
+import Button from '../UI/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import styles from './Section.module.css';
 
-const Section = ({ children }) => {
+const Section = ({ children, onMenuEnable }) => {
   const { darkTheme } = useContext(ThemeContext);
 
   return (
     <section className={darkTheme ? styles.dark : styles.section}>
       {children}
+      <Button
+        className={`${styles.add} ${
+          darkTheme ? styles.darkBtn : styles.lightBtn
+        }`}
+        onClick={onMenuEnable}
+        title={'Add a new task!'}
+      >
+        <FontAwesomeIcon icon={faPlus} />
+      </Button>
     </section>
   );
 };
