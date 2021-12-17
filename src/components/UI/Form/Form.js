cf36cf293c15b64060f5e1dc7d4dc0f2f8dca905
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-17 21:59:14 +0200

    Refactored form to dynamically generate inputs based on props & pass down an additional className to button

--- src/components/UI/Form/Form.js
+++ src/components/UI/Form/Form.js
@@ -1,14 +1,33 @@
 import Button from '../Button/Button';
 import Search from '../Search/Search';
 import styles from './Form.module.css';
+import uniqid from 'uniqid';
 
-const Form = ({ onSubmit, isFieldActive, children, placeholder, type }) => {
+const Form = ({
+  className,
+  buttonClassName,
+  onSubmit,
+  isFieldActive,
+  formData,
+  children,
+  type,
+}) => {
+  const inputLayout = formData.map(({ labelStatus, label, placeholder }) => (
+    <div key={uniqid()} className={labelStatus ? styles.flex : null}>
+      {labelStatus && <label>{label}</label>}
+      <Search className={styles.search} placeholder={placeholder} />
+    </div>
+  ));
+
   return (
-    <form onSubmit={onSubmit}>
-      {isFieldActive && (
-        <Search className={styles.search} placeholder={placeholder} />
-      )}
-      <Button type={type}>{children}</Button>
+    <form onSubmit={onSubmit} className={className}>
+      {isFieldActive && inputLayout}
+      <Button
+        className={`${styles.btn} ${buttonClassName ? buttonClassName : null}`}
+        type={type}
+      >
+        {children}
+      </Button>
     </form>
   );
 };
