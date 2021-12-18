4900e453a524f621263c9d202b9c4eba296d42d9
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-18 18:32:21 +0200

    Passing props to the Modal & AddTaskForm button for the menu to be toggled off. Also minor typo fix to the placeholder

--- src/components/Tasks/AddTaskForm.js
+++ src/components/Tasks/AddTaskForm.js
@@ -1,19 +1,29 @@
 import Modal from '../UI/Modal/Modal';
 import Form from '../UI/Form/Form';
+import Button from '../UI/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import styles from './AddTaskForm.module.css';
 
 // TODO change bg based on context;
 
-const AddTaskForm = (props) => {
+const AddTaskForm = ({ onMenuDisable }) => {
   return (
-    <Modal>
+    <Modal onMenuDisable={onMenuDisable}>
+      <Button
+        onClick={onMenuDisable}
+        title="Close menu"
+        className={styles.ctaSec}
+      >
+        <FontAwesomeIcon icon={faTimes} />
+      </Button>
       <Form
         className={styles.addTaskForm}
         buttonClassName={styles.cta}
         isFieldActive={true}
         formData={[
           {
-            placeholder: 'What are we adding',
+            placeholder: 'What task are we adding?',
             labelStatus: true,
             label: 'Task Name',
           },
