99355ef8d2d9190e31fa5bf3d70b327a1c563f16
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-18 18:37:12 +0200

    Adding the menuEnable & menuDisable handlers and passing them as props

--- src/App.js
+++ src/App.js
@@ -4,28 +4,35 @@ import Header from './components/Header/Header';
 import Aside from './components/Aside/Aside';
 import Section from './components/Section/Section';
 import TaskList from './components/Tasks/TaskList';
+import AddTaskForm from './components/Tasks/AddTaskForm';
 
 function App() {
   const ctx = useContext(ThemeContext);
   const [asideStatus, setAsideStatus] = useState(false);
+  const [isMenuEnabled, setMenuEnabled] = useState(false);
 
   const asideHandler = () => {
     setAsideStatus((prevState) => (prevState === false ? true : false));
   };
 
-  const menuHandler = () => {
-    console.log('Menu enabled');
+  const menuEnableHandler = () => {
+    setMenuEnabled(true);
+  };
+
+  const menuDisableHandler = () => {
+    setMenuEnabled(false);
   };
 
   return (
     <>
+      {isMenuEnabled && <AddTaskForm onMenuDisable={menuDisableHandler} />}
       <Header
         onThemeSelection={ctx.onThemeChange}
         onAsideEnable={asideHandler}
       />
       <main className="App">
         {asideStatus && <Aside theme={ctx.darkTheme} />}
-        <Section onMenuEnable={menuHandler}>
+        <Section onMenuEnable={menuEnableHandler}>
           <TaskList isAsideActive={asideStatus} />
         </Section>
       </main>
