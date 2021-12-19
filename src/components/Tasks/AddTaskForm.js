aeeca321990f9004ddd43f61df6e532b8dfa424c
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-20 00:19:31 +0200

    The form now correctly forwards refs up to the App

--- src/components/Tasks/AddTaskForm.js
+++ src/components/Tasks/AddTaskForm.js
@@ -1,22 +1,46 @@
+import { useRef } from 'react';
 import Search from '../UI/Search/Search';
 import Button from '../UI/Button/Button';
 import styles from './AddTaskForm.module.css';
 
 const AddTaskForm = ({ onAddTask }) => {
+  const taskRef = useRef();
+  const priorityRef = useRef();
+
+  const addTaskHandler = (e) => {
+    const task = taskRef.current.value;
+    const priority = priorityRef.current.value;
+    e.preventDefault();
+    if (task.trim().length > 0 && priority.trim().length > 0) {
+      onAddTask({ task: task, priority: priority });
+    } else {
+      return;
+    }
+  };
+
   return (
-    <form onSubmit={onAddTask} className={styles.addTaskForm}>
+    <form onSubmit={addTaskHandler} className={styles.addTaskForm}>
       <div className={styles.formFields}>
         <label htmlFor="taskName">Task Name</label>
-        <Search id={'taskName'} placeholder={'What task are we adding?'} />
+        <Search
+          required
+          ref={taskRef}
+          id={'taskName'}
+          placeholder={'What task are we adding?'}
+        />
       </div>
       <div className={styles.formFields}>
         <label htmlFor="taskPriority">Task Priority</label>
         <Search
+          required
+          ref={priorityRef}
           id={'taskPriority'}
           placeholder={`What is the task's priority?`}
         />
       </div>
-      <Button className={styles.cta}>Add</Button>
+      <Button type="submit" className={styles.cta}>
+        Add
+      </Button>
     </form>
   );
 };
