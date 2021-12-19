8f1862c0f6f9d4ecbfb8abf3b43bcd48ba526ca2
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-20 00:19:12 +0200

    Fixed the ref forwarding

--- src/components/Aside/AsideForm.js
+++ src/components/Aside/AsideForm.js
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import Search from '../UI/Search/Search';
 import Button from '../UI/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,13 +7,25 @@ import uniqid from 'uniqid';
 import styles from './AsideForm.module.css';
 
 const AsideForm = ({ onAddList }) => {
+  const listRef = useRef();
+
+  const submitHandler = (e) => {
+    const newList = listRef.current.value;
+    e.preventDefault();
+    if (newList.trim().length > 0) {
+      onAddList(listRef.current.value);
+    } else {
+      return;
+    }
+  };
+
   return (
-    <form onSubmit={onAddList} className={styles.asideForm}>
+    <form onSubmit={submitHandler} className={styles.asideForm}>
       <div key={uniqid()}>
-        <Search placeholder={`Let's add a list!`} />
+        <Search required ref={listRef} placeholder={`Let's add a list!`} />
       </div>
       <div>
-        <Button>
+        <Button type="submit">
           <FontAwesomeIcon icon={faPlus} />
         </Button>
       </div>
