1b864a648ad2191d13f142ef4f417cf80e91604a
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-19 23:30:12 +0200

    Made the form leaner & cleaned up the code

--- src/components/Tasks/AddTaskForm.js
+++ src/components/Tasks/AddTaskForm.js
@@ -1,47 +1,23 @@
-import { useContext } from 'react';
-import ThemeContext from '../../store/themeContext';
-import Modal from '../UI/Modal/Modal';
-import Form from '../UI/Form/Form';
+import Search from '../UI/Search/Search';
 import Button from '../UI/Button/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import styles from './AddTaskForm.module.css';
 
-// TODO change bg based on context;
-
-const AddTaskForm = ({ onMenuDisable }) => {
-  const { darkTheme } = useContext(ThemeContext);
-
+const AddTaskForm = ({ onAddTask }) => {
   return (
-    <Modal onMenuDisable={onMenuDisable} className={darkTheme && styles.dark}>
-      <Button
-        onClick={onMenuDisable}
-        title="Close menu"
-        className={`${styles.ctaSec} ${darkTheme ? styles.ctaSecDark : null}`}
-      >
-        <FontAwesomeIcon icon={faTimes} />
-      </Button>
-      <Form
-        className={styles.addTaskForm}
-        buttonClassName={`${styles.cta} ${darkTheme ? styles.ctaDark : null}`}
-        isFieldActive={true}
-        formData={[
-          {
-            placeholder: 'What task are we adding?',
-            labelStatus: true,
-            label: 'Task Name',
-          },
-          {
-            placeholder: `What is the task's priority?`,
-            labelStatus: true,
-            label: 'Priority',
-          },
-        ]}
-        type="submit"
-      >
-        {'Add'}
-      </Form>
-    </Modal>
+    <form onSubmit={onAddTask} className={styles.addTaskForm}>
+      <div className={styles.formFields}>
+        <label htmlFor="taskName">Task Name</label>
+        <Search id={'taskName'} placeholder={'What task are we adding?'} />
+      </div>
+      <div className={styles.formFields}>
+        <label htmlFor="taskPriority">Task Priority</label>
+        <Search
+          id={'taskPriority'}
+          placeholder={`What is the task's priority?`}
+        />
+      </div>
+      <Button className={styles.cta}>Add</Button>
+    </form>
   );
 };
 
