2cddc589c0d84e13f00886141e9f5ba44e03dd1c
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-19 23:34:06 +0200

    Search now accepts an id for semantics

--- src/components/UI/Search/Search.js
+++ src/components/UI/Search/Search.js
@@ -1,16 +1,21 @@
+import React from 'react';
 import styles from './Search.module.css';
 
-const Search = ({ className, onChange, placeholder }) => {
-  return (
-    <>
-      <input
-        className={`${styles.inputField} ${className ? className : ''}`}
-        onChange={onChange}
-        type="text"
-        placeholder={placeholder}
-      ></input>
-    </>
-  );
-};
+const Search = React.forwardRef(
+  ({ className, onChange, placeholder, id }, ref) => {
+    return (
+      <>
+        <input
+          ref={ref}
+          className={`${styles.inputField} ${className ? className : ''}`}
+          onChange={onChange}
+          id={id ? id : null}
+          type="text"
+          placeholder={placeholder}
+        ></input>
+      </>
+    );
+  }
+);
 
 export default Search;
