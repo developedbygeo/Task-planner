640021406785d3f9aff07c7afd06727686f4c3d5
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-24 02:45:36 +0200

    Adding dynamic task rendering based on the active tasklist

--- src/components/Tasks/TaskList.js
+++ src/components/Tasks/TaskList.js
@@ -1,13 +1,17 @@
+import { useContext } from 'react';
+import { TaskContext } from '../../store/taskContext';
 import Task from './Task';
 import styles from './TaskList.module.css';
 
-const currentTasks = [
-  { header: 'potatoes', id: Date.now().valueOf(), description: 'yolooo' },
-];
-
 const TaskList = ({ isAsideActive }) => {
-  const tasks = currentTasks.map(({ header, id, description }) => (
-    <Task header={header} key={id} description={description} />
+  const {
+    currentState: { tasksAndLists: allTasks },
+  } = useContext(TaskContext);
+  const activeTasks = allTasks.filter((list) => list.selected === true)[0]
+    .tasks;
+
+  const tasks = activeTasks.map(({ task, priority, id }) => (
+    <Task header={task} description={priority} key={id} />
   ));
 
   return (
