6cf3c6daf8287bf73b4161cbabc9dd32e068e415
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-25 02:09:57 +0200

    Adding the Priority component, that is used to signify each task's priority level

---
+++ src/components/UI/Priority/Priority.js
@@ -0,0 +1,30 @@
+import _ from 'lodash';
+import Card from '../Card/Card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import styles from './Priority.module.css';
+
+const priorityLevelMap = {
+  1: 'Low',
+  2: 'Medium',
+  3: 'High',
+};
+
+const Priority = ({ className, priorityLevel }) => {
+  const evaluatedLevel = [...Array(+priorityLevel).keys()];
+  const parsedLevel = priorityLevelMap[+priorityLevel];
+
+  const parsedIcons = evaluatedLevel.map((level) => {
+    return (
+      <FontAwesomeIcon className={className} key={_.uniqueId()} icon={faStar} />
+    );
+  });
+
+  return (
+    <Card title={parsedLevel} className={styles.priorityWrapper}>
+      {parsedIcons}
+    </Card>
+  );
+};
+
+export default Priority;
