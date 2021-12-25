020bd02835a8962b54340210194cf665a47a09dd
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-25 02:13:19 +0200

    Adding conditional styling based on the theme ctx

--- src/components/Aside/List.js
+++ src/components/Aside/List.js
@@ -1,11 +1,19 @@
+import { useContext } from 'react';
+import ThemeContext from '../../store/themeContext';
 import Card from '../UI/Card/Card';
 import styles from './List.module.css';
 
 const List = ({ title, selected, id }) => {
+  const { darkTheme } = useContext(ThemeContext);
+  const selectedDark = selected && darkTheme;
+
   return (
     <li
       key={id}
-      className={`${styles.listItems} ${selected ? styles.selected : null}`}
+      className={`${styles.listItems}  ${
+        darkTheme ? styles.darkListItems : null
+      } ${selectedDark && styles.selectedDark} 
+      ${selected ? styles.selected : null}`}
     >
       <Card>
         <p>{title}</p>
