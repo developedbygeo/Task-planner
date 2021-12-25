f029ab28cad46e1e85c10b02800ba75f571bf8c5
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-25 02:19:55 +0200

    Aside now uses the task ctx and renders all added lists within a ul

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,19 +1,31 @@
 import { useContext } from 'react';
 import ThemeContext from '../../store/themeContext';
+import { TaskContext } from '../../store/taskContext';
+import _ from 'lodash';
 import AsideForm from './AsideForm';
+import List from './List';
 import Card from '../UI/Card/Card';
 import styles from './Aside.module.css';
 
-const Aside = ({ onFormReset, children }) => {
+const Aside = ({ onFormReset }) => {
   const { darkTheme } = useContext(ThemeContext);
+  const {
+    currentState: { tasksAndLists: allLists },
+  } = useContext(TaskContext);
 
   return (
     <aside className={darkTheme ? styles.dark : styles.aside}>
-      <Card className={styles.title}>
-        <h3>Current Lists</h3>
-      </Card>
-      <AsideForm onFormReset={onFormReset} />
-      <Card>{children}</Card>
+      <div className={styles.container}>
+        <Card className={styles.title}>
+          <h3>Current Lists</h3>
+        </Card>
+        <AsideForm onFormReset={onFormReset} />
+      </div>
+      <ul>
+        {allLists.map((list) => (
+          <List title={list.list} selected={list.selected} key={_.uniqueId()} />
+        ))}
+      </ul>
     </aside>
   );
 };
