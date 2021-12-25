038c83624a6be7c6fb58785581fe8818408c9bc2
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-25 02:12:10 +0200

    Task now uses the theme context and renders Priority

--- src/components/Tasks/Task.js
+++ src/components/Tasks/Task.js
@@ -1,11 +1,22 @@
+import { useContext } from 'react';
+import ThemeContext from '../../store/themeContext';
+import Priority from '../UI/Priority/Priority';
 import Card from '../UI/Card/Card';
 import styles from './Task.module.css';
 
 const Task = ({ header, id, description }) => {
+  const { darkTheme } = useContext(ThemeContext);
+
   return (
     <li className={styles.li} key={id}>
       <Card className={styles.elementWrapper}>
-        {header ? <h3>{description}</h3> : <h4>{description}</h4>}
+        {header && <h3>{header}</h3>}
+        {description && (
+          <Priority
+            className={darkTheme ? styles.starsDark : styles.stars}
+            priorityLevel={description}
+          />
+        )}
       </Card>
     </li>
   );
