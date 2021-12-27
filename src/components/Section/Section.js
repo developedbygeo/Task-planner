8427ca96d7e1b0b666cef09cf68c0fb6320e1d2a
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-27 02:57:45 +0200

    Added the logic to extract the marked tasks from context and render the respective button conditionally

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,25 +1,51 @@
 import { useContext } from 'react';
 import ThemeContext from '../../store/themeContext';
+import { TaskContext } from '../../store/taskContext';
 import Button from '../UI/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 import styles from './Section.module.css';
 
-const Section = ({ children, onMenuEnable }) => {
+const Section = ({ children, onMenuEnable, onRemoveMenuEnable }) => {
   const { darkTheme } = useContext(ThemeContext);
+  const {
+    currentState: { tasksAndLists },
+  } = useContext(TaskContext);
+
+  const markedTasksExist = tasksAndLists
+    .find((list) => list.selected)
+    .tasks.find((task) => task.completed)
+    ? true
+    : false;
 
   return (
     <section className={darkTheme ? styles.dark : styles.section}>
       {children}
-      <Button
-        className={`${styles.add} ${
-          darkTheme ? styles.darkBtn : styles.lightBtn
+      <div
+        className={`${styles.actionsWrapper} ${
+          markedTasksExist ? styles.actionsWrapperDel : ''
         }`}
-        onClick={onMenuEnable}
-        title={'Add a new task!'}
       >
-        <FontAwesomeIcon icon={faPlus} />
-      </Button>
+        <Button
+          className={`${styles.actionBtn} ${styles.add} ${
+            darkTheme ? styles.darkBtn : styles.lightBtn
+          }`}
+          onClick={onMenuEnable}
+          title={'Add a new task!'}
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </Button>
+        {markedTasksExist && (
+          <Button
+            onClick={onRemoveMenuEnable}
+            className={`${styles.actionBtn} ${styles.delete} ${
+              darkTheme ? styles.darkBtn : styles.lightBtn
+            }`}
+          >
+            <FontAwesomeIcon icon={faTrash} />
+          </Button>
+        )}
+      </div>
     </section>
   );
 };
