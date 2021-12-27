7e90875fcd8745e25d35732d4631698854d61acc
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-27 02:56:09 +0200

    Refactored the component to accept the true prop. Based on this, the content within is rendered dynamically (either the AddTaskForm or the RemoveTaskDialogue) - making it much more modular

--- src/components/Tasks/AddTask.js
+++ src/components/Tasks/AddTask.js
@@ -1,17 +1,18 @@
 import { useContext } from 'react';
 import ThemeContext from '../../store/themeContext';
 import AddTaskForm from './AddTaskForm';
+import RemoveTaskDialogue from './RemoveTaskDialogue';
 import Modal from '../UI/Modal/Modal';
 import Button from '../UI/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import styles from './AddTask.module.css';
 
-const AddTask = ({ onFormReset, onMenuDisable }) => {
+const AddTask = ({ onFormReset, onMenuDisable, add }) => {
   const { darkTheme } = useContext(ThemeContext);
 
-  return (
-    <Modal onMenuDisable={onMenuDisable} className={darkTheme && styles.dark}>
+  const addingLayout = (
+    <>
       <Button
         onClick={onMenuDisable}
         className={`${styles.ctaSec} ${darkTheme ? styles.ctaSecDark : null}`}
@@ -19,6 +20,26 @@ const AddTask = ({ onFormReset, onMenuDisable }) => {
         <FontAwesomeIcon icon={faTimes} />
       </Button>
       <AddTaskForm onFormReset={onFormReset} />
+    </>
+  );
+
+  const removingLayout = (
+    <>
+      <RemoveTaskDialogue onRemoveMenuDisable={onMenuDisable} />
+    </>
+  );
+
+  return (
+    <Modal onMenuDisable={onMenuDisable} className={darkTheme && styles.dark}>
+      {add && addingLayout}
+      {!add && removingLayout}
+      {/* <Button
+          onClick={onMenuDisable}
+          className={`${styles.ctaSec} ${darkTheme ? styles.ctaSecDark : null}`}
+        >
+          <FontAwesomeIcon icon={faTimes} />
+        </Button>
+        <AddTaskForm onFormReset={onFormReset} /> */}
     </Modal>
   );
 };
