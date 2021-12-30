6772d3b2048c18966523135162137d60b4b082ed
georgiosan <georgiosanagnostou@hotmail.com>
2021-12-31 00:41:32 +0200

    Adding the useEffect hook to change state and animate the proper component based on selection & id match

--- src/components/Tasks/Task.js
+++ src/components/Tasks/Task.js
@@ -1,18 +1,33 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import ThemeContext from '../../store/themeContext';
+import { TaskContext } from '../../store/taskContext';
 import Priority from '../UI/Priority/Priority';
 import Card from '../UI/Card/Card';
 import styles from './Task.module.css';
 
 const Task = ({ onClick, completed, title, header, id, description }) => {
   const { darkTheme } = useContext(ThemeContext);
+  const {
+    currentState: { selection },
+  } = useContext(TaskContext);
+  const [isTaskSelected, setTaskSelected] = useState(false);
+
+  useEffect(() => {
+    if (selection === id) {
+      setTaskSelected(true);
+      const timer = setTimeout(() => {
+        setTaskSelected(false);
+      }, 800);
+      return () => clearTimeout(timer);
+    }
+  }, [selection, id]);
 
   return (
     <li onClick={onClick} className={styles.li} key={id} title={title}>
       <Card
         className={`${styles.elementWrapper} ${
           completed ? styles.completed : ''
-        }`}
+        } ${isTaskSelected ? styles.shake : ''}`}
       >
         {header && <h3>{header}</h3>}
         {description && (
